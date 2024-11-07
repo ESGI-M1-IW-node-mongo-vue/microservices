febf6053973712f798566030e90c06ed4d50225d
Romain Grange <romain.grange@pixlstudio.fr>
2024-11-07 15:41:49 +0100

    add livraison

--- front/src/views/Livraison.vue
+++ front/src/views/Livraison.vue
@@ -1,22 +1,199 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
-    <p>Livraison</p>
-    {{orders}}
+  <div class="h-screen flex items-center justify-center flex-col gap-y-10">
+    <div class="bg-white shadow-lg rounded-lg p-6 w-full max-w-3xl">
+      <p class="text-lg font-medium mb-4">Commandes prêtes :</p>
+      <table class="w-full border-collapse">
+        <thead>
+        <tr>
+          <th class="px-4 py-2 border-b font-medium text-left">ID</th>
+          <th class="px-4 py-2 border-b font-medium text-left">Status</th>
+          <th class="px-4 py-2 border-b font-medium text-left"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in ordersReady" class="border-b hover:bg-gray-100">
+          <td class="px-4 py-2 text-left">{{item._id}}</td>
+          <td class="px-4 py-2 text-left">
+            <span :class="{
+              'bg-green-500 text-white px-2 py-1 rounded': item.status === 'ready',
+              'bg-yellow-500 text-white px-2 py-1 rounded': item.status === 'isindelivery',
+              'bg-gray-500 text-white px-2 py-1 rounded': item.status === 'delivered'
+            }">
+              {{ stringStatus(item.status) }}
+            </span>
+          </td>
+          <td class="px-4 py-2 text-left">
+            <button @click="takeOrder(item._id)" class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+              Prendre la commande
+            </button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="bg-white shadow-lg rounded-lg p-6 w-full max-w-3xl">
+      <p class="text-lg font-medium mb-4">Commandes en cours de livraison :</p>
+      <table class="w-full border-collapse">
+        <thead>
+        <tr>
+          <th class="px-4 py-2 border-b font-medium text-left">ID</th>
+          <th class="px-4 py-2 border-b font-medium text-left">Status</th>
+          <th class="px-4 py-2 border-b font-medium text-left"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in ordersisInDelivery" class="border-b hover:bg-gray-100">
+          <td class="px-4 py-2 text-left">{{item._id}}</td>
+          <td class="px-4 py-2 text-left">
+            <span :class="{
+              'bg-green-500 text-white px-2 py-1 rounded': item.status === 'ready',
+              'bg-yellow-500 text-white px-2 py-1 rounded': item.status === 'isindelivery',
+              'bg-gray-500 text-white px-2 py-1 rounded': item.status === 'delivered'
+            }">
+              {{ stringStatus(item.status) }}
+            </span>
+          </td>
+          <td class="px-4 py-2 text-left">
+            <button @click="orderIsDelivered(item._id)" class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+              Commande livrée
+            </button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="bg-white shadow-lg rounded-lg p-6 w-full max-w-3xl">
+      <p class="text-lg font-medium mb-4">Commandes livrés :</p>
+      <table class="w-full border-collapse">
+        <thead>
+        <tr>
+          <th class="px-4 py-2 border-b font-medium text-left">ID</th>
+          <th class="px-4 py-2 border-b font-medium text-left">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in ordersDelivered" class="border-b hover:bg-gray-100">
+          <td class="px-4 py-2 text-left">{{item._id}}</td>
+          <td class="px-4 py-2 text-left">
+            <span :class="{
+              'bg-green-500 text-white px-2 py-1 rounded': item.status === 'ready',
+              'bg-yellow-500 text-white px-2 py-1 rounded': item.status === 'isindelivery',
+              'bg-gray-500 text-white px-2 py-1 rounded': item.status === 'delivered'
+            }">
+              {{ stringStatus(item.status) }}
+            </span>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="absolute top-5 left-4">
+      <button @click="addOrder()" class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+        Ajouter commande
+      </button>
+    </div>
   </div>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue'
+import {ref} from 'vue'
 
-const orders = ref(null)
+const ordersReady = ref(null)
+const ordersisInDelivery = ref(null)
+const ordersDelivered = ref(null)
 
-const getOrders = async function () {
-  await fetch('http://127.0.0.1:8000/livraison/api/order',{
+const getReadyOrders = async function () {
+  await fetch('http://127.0.0.1:8000/livraison/api/order/ready',{
     method:"get",
-
-  }).then((res) => {
-    console.log(res)
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  }).then(async(res) => {
+    ordersReady.value = await res.json()
   })
 }
 
-getOrders()
+const getIsInDeliveryOrders = async function () {
+  await fetch('http://127.0.0.1:8000/livraison/api/order/isindelivery',{
+    method:"get",
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  }).then(async(res) => {
+    ordersisInDelivery.value = await res.json()
+  })
+}
+
+const getDeliveredOrders = async function () {
+  await fetch('http://127.0.0.1:8000/livraison/api/order/delivered',{
+    method:"get",
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  }).then(async(res) => {
+    ordersDelivered.value = await res.json()
+  })
+}
+
+const stringStatus = function (status){
+  switch (status){
+    case 'ready':
+      return 'Prête'
+    case 'isindelivery':
+      return 'En cours de livraison'
+    case 'delivered':
+      return 'Livré'
+    default:
+      return 'Prête'
+  }
+}
+
+const takeOrder = async function (orderId){
+  await fetch(`http://127.0.0.1:8000/livraison/api/order/${orderId}`,{
+    method:"put",
+    body:JSON.stringify({"status":"isindelivery"}),
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  })
+  setTimeout(() => {
+    getReadyOrders()
+    getIsInDeliveryOrders()
+    getDeliveredOrders()
+  }, 200)
+}
+
+const orderIsDelivered = async function (orderId){
+  await fetch(`http://127.0.0.1:8000/livraison/api/order/${orderId}`,{
+    method:"put",
+    body:JSON.stringify({"status":"delivered"}),
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  })
+  setTimeout(() => {
+    getReadyOrders()
+    getIsInDeliveryOrders()
+    getDeliveredOrders()
+  }, 200)
+}
+
+const addOrder = async function (){
+  const data = new FormData();
+  data.append("status", "ready");
+
+  await fetch("http://127.0.0.1:8000/livraison/api/order",{
+    method:"post",
+    body:data,
+    headers: {
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoIiwic3ViIjoiNjZkZWY3MWVkM2Q3NGVhMzNmMjBjMWJlIiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzEwNzMyNTJ9.0a5s-SEUbGv8uSqKKjCmQ6cu7ygqPI8jdQYHJe8LHU8"
+    }
+  })
+  setTimeout(() => {
+    getReadyOrders()
+  }, 200)
+}
+
+getReadyOrders()
+getIsInDeliveryOrders()
+getDeliveredOrders()
 </script>
