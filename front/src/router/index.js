cb42d399fb9a75f94c3b3082e076c6e679ff1d76
Romain Grange <romain.grange@pixlstudio.fr>
2024-11-07 10:17:51 +0100

    start microservice livraison

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,25 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
+import Livraison from "@/pages/Livraison.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
+      {
       path: '/login',
       name: 'login',
       component: LoginView
@@ -28,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/livraison',
+      name: 'livraison',
+      component: Livraison
     },
   ]
 })
